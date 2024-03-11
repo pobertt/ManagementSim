e7195e7bdf523779b60232b23bb1fde85c8c2b16
pobertt <79865756+pobertt@users.noreply.github.com>
2024-03-11 17:08:47 +0000

    Added CPP ingredient

---
+++ Source/ManagementSim/IngredientsActor.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "IngredientsActor.generated.h"
+
+UCLASS()
+class MANAGEMENTSIM_API AIngredientsActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AIngredientsActor();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/* Getter functions for Components */
+
+	FORCEINLINE UBoxComponent* GetBoxComponent() const
+	{
+		return _BoxComponent;
+	}
+
+	FORCEINLINE UStaticMeshComponent* GetMeshComponent() const
+	{
+		return _MeshComponent;
+	}
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ingredients Component", meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* _BoxComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ingredients Component", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* _MeshComponent;
+};
